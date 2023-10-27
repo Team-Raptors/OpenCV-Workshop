c9b18f2c76f391528fcb076bd06007b828af4c20
Team Raptors <68473120+Team-Raptors@users.noreply.github.com>
2023-10-27 21:47:56 +0530

    Update addition_subtraction_image.py

--- code_files/addition_subtraction_image.py
+++ code_files/addition_subtraction_image.py
@@ -1,22 +1,24 @@
-# Python add
-# arithmetic operation of 
-# organizing imports 
-import cv2 
-import numpy as np 
-	
-image1 = cv2.imread('Images/add input1.jpg') 
-image2 = cv2.imread('Images/add input 2.jpg') 
+import cv2 as cv  # Import OpenCV and alias it as cv
+import numpy as np
 
-cv2.imshow('img1', image1)
-cv2.imshow('img2', image2)
+# Load two images
+image1 = cv.imread('Images/add input1.jpg')
+image2 = cv.imread('Images/add input 2.jpg')
 
-weightedSum = cv2.addWeighted(image1, 0.5, image2, 0.4, 0) 
-sub = cv2.subtract(image1, image2)
+# Display the loaded images
+cv.imshow('img1', image1)
+cv.imshow('img2', image2)
 
+# Perform weighted addition of the two images
+weightedSum = cv.addWeighted(image1, 0.5, image2, 0.4, 0)
 
-cv2.imshow('Weighted Image', weightedSum) 
-cv2.imshow('Subtracted Image', sub)
+# Subtract image2 from image1
+sub = cv.subtract(image1, image2)
 
-# De-allocate any associated memory usage 
-if cv2.waitKey(0) & 0xff == 27: 
-	cv2.destroyAllWindows() 
+# Display the resulting images
+cv.imshow('Weighted Image', weightedSum)
+cv.imshow('Subtracted Image', sub)
+
+# Wait for a key press and close the OpenCV windows
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
